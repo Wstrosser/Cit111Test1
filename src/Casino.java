ef4f98cbad9541e3db14129ae558e77b4e390db2
Wstrosser <44884519+Wstrosser@users.noreply.github.com>
2018-11-08 22:40:29 -0500

    Update src/Casino.java

--- src/Casino.java
+++ src/Casino.java
@@ -9,7 +9,7 @@ public class Casino {
 		while(illegal==false) {
 		y=card.cardDealtType();
 		x=card.cardDealtValue();
-			System.out.println(card.cardName(card.cardDealtValue(), card.cardDealtType()));
+			System.out.println(card.cardName(x,y));
 			Thread.sleep(10);
 			illegal = card.cardIllegalChecker(x, y);
 	}
